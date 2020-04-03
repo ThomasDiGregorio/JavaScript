3b5273d6161cef88feca70a9cd6377a4d792b52e
ThomasDiGregorio <digregoriothomas@yahoo.com>
2020-04-03 13:26:36 -0400

    spread operator

--- modern.js
+++ modern.js
@@ -1,2 +1,17 @@
 const city = 'Scotsdale';
 console.log(city);
+
+__________________________
+
+function Invoice(subTotal) {
+    this.taxRate = 0.06;
+    this.subTotal = subTotal;
+  
+    this.total = setInterval(() => {
+      console.log((this.taxRate * this.subTotal) + this.subTotal);
+      // console.log(this);
+    }, 2000);
+  }
+  
+  const inv = new Invoice(200);
+  inv.total();
